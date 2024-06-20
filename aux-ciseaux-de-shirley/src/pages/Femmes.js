50e4d39d541ac7820b7aa10d339554c1500e4350
Frédéric.Toppan <toppan.frederic@gmail.com>
2024-06-20 17:43:29 +0200

    stand by photos hommes et garcons

--- aux-ciseaux-de-shirley/src/pages/Femmes.js
+++ aux-ciseaux-de-shirley/src/pages/Femmes.js
@@ -1,16 +1,13 @@
-// src/pages/Femmes.js
 import React from 'react';
-import Carre1 from '../images/carre1.webp';
-import Carre2 from '../images/carre2.webp';
-import Carre3 from '../images/carre3.webp';
+import photos from '../photos.json';
 
 const Femmes = () => (
   <div>
     <h3 className="titlesection">Femmes</h3>
-    <section className="chignon" id="carre">
-      <img className="image" src={Carre1} alt="1er carré" />
-      <img className="image" src={Carre2} alt="2eme carré" />
-      <img className="image" src={Carre3} alt="3eme carré" />
+    <section className="chignon">
+      {photos.femmes.map((photo, index) => (
+        <img key={index} className="image" src={photo.src} alt={photo.alt} />
+      ))}
     </section>
   </div>
 );
